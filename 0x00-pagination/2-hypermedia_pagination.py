282d1ba5300c3e0bb2d6b666f8a9f07b4dcc86ed
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-03 00:20:34 +0100

    update task 2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -51,13 +51,14 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """ Return a dictionary"""
-        data = self.get_dataset(page, page_size)
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
         total_pages = math.ceil(len(self.__dataset) / page_size)
         return {
-            "page_size": len(data),
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if page < total_pages else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_pages": total_pages
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
         }
