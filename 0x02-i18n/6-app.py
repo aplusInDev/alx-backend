442606d2f2ca672d437eb10be28eb0b8051b7b57
aplusJsDev <laabid.abdessamadplus@gmail.com>
2024-02-07 02:53:41 +0100

    test6

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,65 +1,66 @@
 #!/usr/bin/env python3
-"""Basic Babel setup"""
-
+'''Basic app with only single route'''
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
-from typing import Dict, Union
-
-app = Flask(__name__)
-
-babel = Babel(app)
 
 
 class Config:
-    """Config app class"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    '''Class for babel configuration'''
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
+babel = Babel(app)
 app.config.from_object(Config)
 
-
-@app.route('/')
-def index() -> str:
-    """Index page"""
-    return render_template('4-index.html')
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """Get locale"""
-    locale = request.args.get('locale', '')
-    if locale in app.config["LANGUAGES"]:
-        return locale
-    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
-        return g.user['locale']
-    header_locale = request.headers.get('locale', '')
-    if header_locale in app.config["LANGUAGES"]:
-        return header_locale
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
-def get_user() -> Union[Dict, None]:
-    """Get user"""
-    login_as = request.args.get('login_as')
-    if login_as:
-        return {
-            "name": "Balou",
-            "locale": "fr",
-            "timezone": "Europe/Paris",
-        }
+def get_user():
+    '''Mocks logged in user'''
+    ID = request.args.get('login_as')
+    if ID and int(ID) in users:
+        return users[int(ID)]
     return None
 
 
 @app.before_request
-def before_request() -> None:
-    """Before request"""
+def before_request():
+    '''Before request'''
     user = get_user()
-    if user:
-        from flask import g
-        g.user = user
+    g.user = user
+
+
+@babel.localeselector
+def get_locale():
+    '''Gets the locale language'''
+    url_locale = request.args.get('locale', None)
+    if url_locale and url_locale in app.config['LANGUAGES']:
+        return url_locale
+
+    user_locale = g.user['locale']
+    if user_locale and user_locale in app.config['LANGUAGES']:
+        return user_locale
+
+    header_locale = request.headers.get('locale', None)
+    if header_locale and header_locale in app.config['LANGUAGES']:
+        return header_locale
+
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    '''Basic index page'''
+    return render_template('6-index.html', user=g.user)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port="5000", host="0.0.0.0", debug=True)
